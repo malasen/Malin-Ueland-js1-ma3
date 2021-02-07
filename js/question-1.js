4a6955a699957e2436e2c65fedd371b9f7d70e71
malasen <71284217+malasen@users.noreply.github.com>
2021-02-07 19:36:38 +0100

    script edit

---
+++ js/question-1.js
@@ -0,0 +1,13 @@
+// Question 1
+
+
+/*
+function division(a, b) {    
+    return a % b;
+}
+*/
+
+const division = (a, b) => a % b;
+
+//console.log(division(100, 3));
+
--- js/question-2.js
+++ js/question-2.js
@@ -1,17 +1,3 @@
-// Question 1
-
-
-/*
-function division(a, b) {    
-    return a % b;
-}
-*/
-
-const division = (a, b) => a % b;
-
-//console.log(division(100, 3));
-
-
 //Question 2
 
 const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
